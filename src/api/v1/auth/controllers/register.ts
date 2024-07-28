7604e4b881f8e58e031af1813622f1a5df45f253
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2024-07-28 16:33:02 +0530

    Modifies register.ts to only contain register code

--- src/api/v1/auth/controllers/register.ts
+++ src/api/v1/auth/controllers/register.ts
@@ -54,11 +54,7 @@ passport.deserializeUser((user, done) => {
 // @desc - Register User
 // @route - POST /api/v1/auth/register
 // @access - Public
-export const register = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const register = async (req: Request, res: Response, next: NextFunction) => {
   const { username, email } = req.body;
 
   if (!email) {
@@ -113,57 +109,4 @@ export const register = async (
   }
 };
 
-// @desc    Signin user
-// @route   POST /api/v1/auth/signin
-// @access  Public
-export const signin = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const { email } = req.body;
-
-  if (!email) {
-    return res.status(400).json({
-      success: false,
-      data: {
-        name: 'Please provide your email address',
-      },
-    });
-  }
-
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    return res.status(400).json({
-      statusCode: 400,
-      success: false,
-      data: {
-        name: 'Invalid Credentials',
-      },
-    });
-  }
-
-  try {
-    const verificationCode = user.getVerificationCode!();
-    await user.save();
-
-    sendEmail(
-      email,
-      `${process.env.APP_NAME} confirmation code`,
-      verificationHtml(verificationCode)
-    );
-
-    res.status(201).json({
-      success: true,
-      data: {
-        name: 'Verification code sent to email',
-      },
-    });
-  } catch (err) {
-    user.loginVerificationCode = undefined;
-    user.loginVerificationCodeExpires = undefined;
-    await user.save({ validateBeforeSave: false });
-    next(err);
-  }
-};
+export default register;
